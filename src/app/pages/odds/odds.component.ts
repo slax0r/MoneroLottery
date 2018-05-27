e6c53659c6b14143cf3326d7ad5bab08b279260a
slax0r <frcolejr@gmail.com>
2018-05-27 00:26:36 -0400

    bump 3

---
+++ src/app/pages/odds/odds.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-odds',
+  templateUrl: './odds.component.html',
+  styleUrls: ['./odds.component.css']
+})
+export class OddsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
